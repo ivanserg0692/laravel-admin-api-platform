107b431a901b86e0fc2155d78b927bedaa80b6f4
ivan <ivan.serg0692@gmail.com>
2026-02-14 19:59:39 +0800

    changes after the installation of Breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 import flowbitePlugin from "flowbite/plugin";
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-      //it's for Flowbite
-      "./node_modules/flowbite/**/*.js",
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        //it's for Flowbite
+        "./node_modules/flowbite/**/*.js",
+        // it's optional for laravel pagination
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
+    ],
 
-      // it's optional for laravel pagination
-      "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      flowbitePlugin,
-  ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, flowbitePlugin],
 };
