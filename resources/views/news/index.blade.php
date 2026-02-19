c2f7890675927a3f3b9cb6127acba8f90894e7ec
ISPetrov <petrov.is@gutagroup.ru>
2026-02-19 22:58:08 +0800

    TASK-004 feature: the table slots and columns support

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,4 +1,14 @@
-@props(['news'])
 <x-app-layout>
-    <x-crud.index :items="$news"></x-crud.index>
+    <x-crud.index
+        :items="$news"
+        :columns="[
+            ['key' => 'id', 'label' => 'ID'],
+            ['key' => 'title', 'label' => 'Title'],
+            ['key' => 'status', 'label' => 'Status'],
+            ['key' => 'published_at', 'label' => 'Published'],
+            ['key' => 'author_id', 'label' => 'Author'],
+            ['key' => 'views_count', 'label' => 'Views'],
+        ]"
+    >
+    </x-crud.index>
 </x-app-layout>
