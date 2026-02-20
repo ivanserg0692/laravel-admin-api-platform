4615ca7d5df0f6e08fe8cacccbdfb22af5790e55
ISPetrov <petrov.is@gutagroup.ru>
2026-02-21 00:40:40 +0800

    TASK-004 feature: the new panel modal

--- resources/views/components/crud/modals/read-product.blade.php
+++ resources/views/components/crud/modals/read-product.blade.php
@@ -1,48 +1,37 @@
-<x-modal name="read-product" maxWidth="xl">
-    <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-        <div class="flex justify-between mb-4 rounded-t sm:mb-5">
-            <div class="text-lg text-gray-900 md:text-xl dark:text-white">
-                <h3 class="font-semibold">Apple iMac 27”</h3>
-                <p class="font-bold">$2999</p>
-            </div>
-            <div>
-                <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white"
-                        x-on:click="$dispatch('close-modal', 'read-product')">
-                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
+<x-modals.panel name="read-product" maxWidth="xl" headerClass="flex items-start justify-between mb-4 rounded-t sm:mb-5">
+    <x-slot:header>
+        <div class="text-lg text-gray-900 md:text-xl dark:text-white">
+            <h3 class="font-semibold">Apple iMac 27”</h3>
+            <p class="font-bold">$2999</p>
         </div>
-        <dl>
-            <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Details</dt>
-            <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">
-                Standard glass ,3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US.
-            </dd>
-            <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Category</dt>
-            <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">Electronics/PC</dd>
-        </dl>
-        <div class="flex justify-between items-center">
-            <div class="flex items-center space-x-3 sm:space-x-4">
-                <x-buttons.primary type="button" class="px-5 py-2.5">
-                    <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"/>
-                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"/>
-                    </svg>
-                    Edit
-                </x-buttons.primary>
-                <x-buttons.secondary type="button" class="px-5 py-2.5">
-                    Preview
-                </x-buttons.secondary>
-            </div>
-            <x-buttons.danger type="button" class="px-5 py-2.5">
-                <svg aria-hidden="true" class="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/>
+    </x-slot:header>
+
+    <dl>
+        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Details</dt>
+        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">
+            Standard glass ,3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US.
+        </dd>
+        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Category</dt>
+        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">Electronics/PC</dd>
+    </dl>
+    <div class="flex justify-between items-center">
+        <div class="flex items-center space-x-3 sm:space-x-4">
+            <x-buttons.primary type="button" class="px-5 py-2.5">
+                <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"/>
+                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"/>
                 </svg>
-                Delete
-            </x-buttons.danger>
+                Edit
+            </x-buttons.primary>
+            <x-buttons.secondary type="button" class="px-5 py-2.5">
+                Preview
+            </x-buttons.secondary>
         </div>
+        <x-buttons.danger type="button" class="px-5 py-2.5">
+            <svg aria-hidden="true" class="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/>
+            </svg>
+            Delete
+        </x-buttons.danger>
     </div>
-</x-modal>
+</x-modals.panel>
