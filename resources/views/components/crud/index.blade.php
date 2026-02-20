3647377f16f96aa41d8f216baad4f53adb866c17
ISPetrov <petrov.is@gutagroup.ru>
2026-02-20 20:00:47 +0800

    TASK-004 feature: todo explanations

--- resources/views/components/crud/index.blade.php
+++ resources/views/components/crud/index.blade.php
@@ -8,29 +8,31 @@
         <!-- Start coding here -->
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
 
+            {{--
+            @todo реализовать tool bar
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-                <div class="w-full md:w-1/2">
-                    <x-forms.search/>
-                </div>
+                      <div class="w-full md:w-1/2">
+                          <x-forms.search/>
+                      </div>
 
-                <div
-                    class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                    <x-buttons.primary type="button" id="createProductModalButton" data-modal-target="createProductModal"
-                                      data-modal-toggle="createProductModal" class="!px-4 !py-2 !font-medium">
-                        <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path clip-rule="evenodd" fill-rule="evenodd"
-                                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
-                        </svg>
-                        Add product
-                    </x-buttons.primary>
+                      <div
+                          class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                          <x-buttons.primary type="button" id="createProductModalButton" data-modal-target="createProductModal"
+                                            data-modal-toggle="createProductModal" class="!px-4 !py-2 !font-medium">
+                              <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
+                                   xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                  <path clip-rule="evenodd" fill-rule="evenodd"
+                                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
+                              </svg>
+                              Add product
+                          </x-buttons.primary>
 
-                    <div class="flex items-center space-x-3 w-full md:w-auto">
-                        <x-crud.toolbar.actions-dropdown/>
-                        <x-crud.toolbar.filter-dropdown/>
-                    </div>
-                </div>
-            </div>
+                          <div class="flex items-center space-x-3 w-full md:w-auto">
+                              <x-crud.toolbar.actions-dropdown/>
+                              <x-crud.toolbar.filter-dropdown/>
+                          </div>
+                      </div>
+                  </div>--}}
 
 
             <x-lists.table
