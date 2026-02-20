1b117a0e7d09d954b10f556c6fef4ea0bd923b81
ISPetrov <petrov.is@gutagroup.ru>
2026-02-20 17:46:01 +0800

    TASK-004 feature: the modal overlays

--- resources/views/components/crud/modals/create-product.blade.php
+++ resources/views/components/crud/modals/create-product.blade.php
@@ -1,7 +1,8 @@
 <!-- Create modal -->
 <div id="createProductModal" tabindex="-1" aria-hidden="true"
      class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-2xl max-h-full">
+    <div data-modal-toggle="createProductModal" class="fixed inset-0 bg-gray-900/50 dark:bg-gray-900/80"></div>
+    <div class="relative z-10 p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
         <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
             <!-- Modal header -->
--- resources/views/components/crud/modals/delete-product.blade.php
+++ resources/views/components/crud/modals/delete-product.blade.php
@@ -1,7 +1,8 @@
 <!-- Delete modal -->
 <div id="deleteModal" tabindex="-1" aria-hidden="true"
      class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
+    <div data-modal-toggle="deleteModal" class="fixed inset-0 bg-gray-900/50 dark:bg-gray-900/80"></div>
+    <div class="relative z-10 p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
         <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
             <button type="button"
