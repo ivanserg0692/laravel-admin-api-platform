9785521bdd22e4e8d787f61408ecc7df35211cf2
ISPetrov <petrov.is@gutagroup.ru>
2026-03-06 00:21:38 +0800

    TASK-007 feature: add event listener and the event generation for news creation notification

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
+use App\Events\NewsCreatedEvent;
+use App\Listeners\SendNewsCreatedNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        NewsCreatedEvent::class => [
+            SendNewsCreatedNotification::class,
         ],
     ];
 
