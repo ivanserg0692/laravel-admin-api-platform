dc8a5b46ca8ed32429ed4081fb2b671ef2011614
ISPetrov <petrov.is@gutagroup.ru>
2026-03-07 18:57:05 +0800

    TASK-008 feature: added the created at column

--- app/Filament/Resources/NewsExports/Tables/NewsExportsTable.php
+++ app/Filament/Resources/NewsExports/Tables/NewsExportsTable.php
@@ -38,12 +38,7 @@ class NewsExportsTable
                     ->searchable(),
                 TextColumn::make('created_at')
                     ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->sortable(),
             ])
             ->filters([
                 //
